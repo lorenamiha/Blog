6d91d0ffce826bb8cbdf09c481336dcfe7970e05
lorenamiha <lorenacasandra524@gmail.com>
2024-05-10 20:15:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function openArticles(){
+    window.location.href='index.html';
+
+}
+
+function openContact(){
+    window.location.href='contact.html';
+
+}
+
+function openReviews(){
+     window.location.href='reviews.html';
+
+}
